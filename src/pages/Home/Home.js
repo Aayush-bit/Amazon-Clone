1a3cd68e340fff3332cf84d9f13b960a491e6d9f
Aayush Mahajan <mahajanaayush23@gmail.com>
2021-10-19 14:55:03 +0530

    added hero image

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Product from '../../components/Product/Product';
+import "./Home.css"
 
 const Home = () => {
+    const img = "https://m.media-amazon.com/images/I/51upXzq22FL._SX1500_.jpg";
+    // const img = "https://m.media-amazon.com/images/I/711Y9Al9RNL._SX3740_.jpg";
+    
     return (
-        <div>
-            home
+        <div className="home">
+            <img 
+            className="home__image" 
+            src={img} />
+            <div className="home__rows">
+                <div className="home__row">
+                    <Product />
+                </div>
+                <div className="home__row">
+                    {/* <Product /> */}
+                </div>
+                <div className="home__row">
+                    {/* <Product /> */}
+                </div>
+            </div>
         </div>
     )
 }

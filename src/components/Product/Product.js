b02bae28d432742b09aaaefca9c5eca855c19b57
Aayush Mahajan <mahajanaayush23@gmail.com>
2021-10-21 23:05:01 +0530

    added more and better stuff into product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,29 +1,40 @@
 import { Button } from '@mui/material';
 import React from 'react'
+import StarIcon from '@mui/icons-material/Star';
 import "./Product.css"
+import { StarBorder } from '@mui/icons-material';
 
 const Product = () => {
-    const title = "Lorem ipsum dolor sit amet, consectetur adipisicing";
+    const title = "VRINDAVANBAZAAR.COM Krishna-Balaram Soft Toy Dolls- 8 inches- Soft and Stuffed Washable Toys";
     const price = 20;
-    const rating = 5
-    const productImage = "image";
-    
+    const numericRating = 4;
+    const productImage = "https://m.media-amazon.com/images/I/61sw43+4gRL._AC_UL480_FMwebp_QL65_.jpg";
+
     return (
         <div className="product">
             {/* product title */}
             <p className="product__title">{title}</p>
             
             {/* product price */}
-            <p className="product__price">{price}</p>
+            <p className="product__price">$ {price}</p>
             
             {/* product rating */}
-            <p className="product__rating">{rating}</p>
+            <p className="product__rating">
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                <StarIcon />
+                {/* <StarBorder /> */}
+            </p>
             
             {/* product image */}
-            <p className="product__image">{productImage}</p>
+            <img className="product__image" src={productImage} alt={title} />
             
             {/* CTA - add to cart */}
-            <Button className="product__addToCart">Add to Cart</Button>
+            <div className="product__addToCart">
+                <button>Add to Cart</button>
+            </div>
         </div>
     )
 }

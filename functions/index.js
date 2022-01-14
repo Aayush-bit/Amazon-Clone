00555a9b534cf690c774a6575d9e1b88eef2b2ea
Aayush Mahajan <mahajanaayush23@gmail.com>
2022-01-14 22:49:22 +0530

    required dotenv

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,27 @@
+require("dotenv").config();
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
+const stripe = require("stripe")(`${ process.env.STRIPE_SECRET_KEY }`);
 
 const app = express();
-// app.use(cors({ origin: true }));
-app.use(cors());
+app.use(cors({ origin: true }));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200).send("working!");
+    res.send("/ route working :)");
 })
 
 app.post('/payments/create', async (req, res) => {
     const total = req.query.total;
-    const paymentIntent = await stripe.paymentIntent.create({
+
+    const paymentIntent = await stripe.paymentIntents.create({
         amount: total,
         currency: "usd"
     });
     
-    res.status(201).send({
+    res.status(201).json({
         clientSecret: paymentIntent.client_secret
     });
 })
